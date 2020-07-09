060428a13063d32548ef0780fafc567084741a2f
Nick Sotula <so2niko@gmail.com>
2020-07-09 19:32:54 +0300

    fix: add programmatically version buyt v3 reCaptcha

--- app/re_captcha_v2.js
+++ app/re_captcha_v2.js
@@ -1,3 +1,15 @@
 function onSubmit(token) {
     document.getElementById("demo-form").submit();
-  }
+}
+
+function onClick(e) {
+  e.preventDefault();
+  grecaptcha.ready(function() {
+    grecaptcha.execute('reCAPTCHA_site_key', {action: 'submit'}).then(function(token) {
+        // Add your logic to submit to your backend server here.
+    });
+  });
+}
+
+
+document.querySelector('#demo-form').addEventListener('submit', onClick);
